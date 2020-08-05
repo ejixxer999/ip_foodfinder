3824ce4282c1af78b0463806628db7582fde6ca4
ejixxer999 <lehman.ethan2@gmail.com>
2020-08-04 20:59:55 -0400

    added my ip api to pull users ip address for further use

--- lib/api.rb
+++ lib/api.rb
@@ -1,5 +1,14 @@
+
+require 'open-uri'
+require 'pry'
+ 
+        
 class API
-    def get_info
+    def get_info 
+         remote_ip = open('http://whatismyip.akamai.com').read
+         puts remote_ip
+        response = HTTParty.get("http://ip-api.com/json/" + remote_ip)
        
     end 
 end 
+API.new.get_info
