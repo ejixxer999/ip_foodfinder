91ab660dadc37eb16065d074c0048a4d37709475
ejixxer999 <lehman.ethan2@gmail.com>
2020-08-06 23:43:39 -0400

    added a second api along with api key, and created a var to pull data from first api

--- lib/api.rb
+++ lib/api.rb
@@ -1,14 +1,38 @@
-
+require 'net/http'
+require 'openssl'
 require 'open-uri'
 require 'pry'
+require 'unirest'
  
         
 class API
-    def get_info 
-         remote_ip = open('http://whatismyip.akamai.com').read
-         puts remote_ip
-        response = HTTParty.get("http://ip-api.com/json/" + remote_ip)
+    
+    remote_ip = open('http://whatismyip.akamai.com').read
+    BASE_URL = "http://ip-api.com/json/" + remote_ip
+    
+    
+    def get_coor_ip
        
-    end 
+         response = HTTParty.get("#{BASE_URL}" + "?fields=lat,lon,city,regionName,timezone")
+         
+         user = User.new(response["regionName"], response["city"], response["timezone"]) 
+         response
+        end 
+        
+    def get_res_data
+        location = get_coor_ip
+
+        response = HTTParty.get("https://us-restaurant-menus.p.rapidapi.com/restaurants/search/geo?page=1&lon=#{location["lon"]}&lat=#{location["lat"]}&distance=1",
+        headers:{
+            "X-RapidAPI-Host" => "us-restaurant-menus.p.rapidapi.com",
+            "X-RapidAPI-Key" => ENV['KRABBY_PATTY']
+         },
+          parameters:{
+             "parameter" => "value"
+             })
+           
+        
+    end
 end 
-API.new.get_info
+API.new.get_coor_ip
+API.new.get_res_data
