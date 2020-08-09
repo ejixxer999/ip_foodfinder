9537004e30c5911914b98c4e9a5867912a91ec2c
ejixxer999 <lehman.ethan2@gmail.com>
2020-08-09 01:54:37 -0400

    added loading screen

---
+++ lib/loading.rb
@@ -0,0 +1,137 @@
+class Loading 
+    def self.frame_one
+        puts "                                                                                 
+        .oooooooddddddsddddddsdddddhydddddyhdddddsdddddhsdddddhsdddddhsdddddh                                       
+        .oooooooddddddsddddddsdddddhydddddyhdddddsddddddsddddddsddddddsdddddd                                       
+        .oooooooddddddsddddddsdddddhydddddyhdddddsddddddsddddddsddddddsdddddd                                       
+        `------.::::::-::::::-::::::-:::::-::::::-::::::-::::::-::::::-:::::: 
+"
+puts "Loading...."
+sleep (0.09)
+    system ("clear")
+    end 
+
+    def self.frame_two
+        puts "                                                                                 
+        -dddddh+oooooooddddddsdddddhydddddyhdddddsdddddhsdddddhsdddddhsdddddh                                       
+        -dddddd+oooooosddddddsdddddhydddddyhdddddsddddddsddddddsddddddsdddddd                                       
+        -dddddd+oooooosddddddsdddddhydddddyhdddddsddddddsddddddsddddddsdddddd                                       
+        `::::::.-------::::::-::::::-:::::-::::::-::::::-::::::-::::::-:::::: 
+"
+puts "Loading...."
+    sleep (0.09)
+    system ("clear")
+    end 
+    
+    def self.frame_three
+        puts "                                                                                 
+        -ddddddsdddddh+oooooosdddddhydddddyhdddddsdddddhsdddddhsdddddhsdddddh                                       
+        -ddddddsdddddd+oooooosdddddhydddddyhdddddsddddddsddddddsddddddsdddddd                                       
+        -ddddddsdddddd+oooooosdddddhydddddyhdddddsddddddsddddddsddddddsdddddd                                       
+        `::::::-::::::.-------::::::-:::::-::::::-::::::-::::::-::::::-::::::"
+        puts "Loading...."
+        sleep (0.09)
+        system ("clear")
+    end 
+
+
+    def self.frame_four
+        puts "                                                                                
+        -ddddddsddddddsdddddh/ooooo+ydddddyhdddddsdddddhsdddddhsdddddhsdddddh                                       
+        -ddddddsddddddsdddddd/ooooooydddddyhdddddsddddddsddddddsddddddsdddddd                                       
+        -ddddddsddddddsdddddd/ooooooydddddyhdddddsddddddsddddddsddddddsdddddd                                       
+        `::::::-::::::-::::::.-----.-:::::-::::::-::::::-::::::-::::::-::::::"
+        puts "Loading...."
+        sleep (0.09)
+        system ("clear")
+    end 
+    
+
+    def self.frame_five
+        puts "                                                                               
+        -ddddddsddddddsddddddsdddddy+ooooo+hdddddsdddddhsdddddhsdddddhsdddddh                                       
+        -ddddddsddddddsddddddsdddddh+ooooo+hdddddsddddddsddddddsddddddsdddddd                                       
+        -ddddddsddddddsddddddsdddddh+ooooo+hdddddsddddddsddddddsddddddsdddddd                                       
+        `::::::-::::::-::::::-::::::.-----.::::::-::::::-::::::-::::::-:::::: "
+        puts "Loading...."
+        sleep (0.09)
+        system ("clear")
+    end 
+
+
+    def self.frame_six
+        puts "                                                                                 
+        -ddddddsddddddsddddddsdddddhyddddds+ooooo+dddddhsdddddhsdddddhsdddddh                                       
+        -ddddddsddddddsddddddsdddddhydddddyoooooo+ddddddsddddddsddddddsdddddd                                       
+        -ddddddsddddddsddddddsdddddhydddddyoooooo+ddddddsddddddsddddddsdddddd                                       
+        `::::::-::::::-::::::-::::::-:::::-.-----.::::::-::::::-::::::-::::::"
+        puts "Loading...."
+        sleep (0.09)
+        system ("clear")
+    end 
+
+
+    def self.frame_seven
+        puts "                                                                             
+        -ddddddsddddddsddddddsdddddhydddddyhddddhooooooo+dddddhsdddddhsdddddh                                       
+        -ddddddsddddddsddddddsdddddhydddddyhdddddsoooooo+ddddddsddddddsdddddd                                       
+        -ddddddsddddddsddddddsdddddhydddddyhdddddsoooooo+ddddddsddddddsdddddd                                       
+        `::::::-::::::-::::::-::::::-:::::-::::::-------.::::::-::::::-:::::: "
+        puts "Loading...."
+        sleep(0.09)
+        system ("clear")
+    end 
+
+    def self.frame_eight 
+        puts "                                                                              
+        -ddddddsddddddsddddddsdddddhydddddyhdddddsdddddhoooooooodddddhsdddddh                                       
+        -ddddddsddddddsddddddsdddddhydddddyhdddddsddddddooooooooddddddsdddddd                                       
+        -ddddddsddddddsddddddsdddddhydddddyhdddddsddddddooooooooddddddsdddddd                                       
+        `::::::-::::::-::::::-::::::-:::::-::::::-::::::.------.::::::-::::::"
+        puts "Loading...."
+        sleep (0.09)
+        system ("clear")
+    end 
+
+
+    def self.frame_nine
+        puts "                                                                            
+        -ddddddsddddddsddddddsdddddhydddddyhdddddsdddddhsdddddh+ooooooodddddh                                       
+        -ddddddsddddddsddddddsdddddhydddddyhdddddsddddddsdddddd+oooooosdddddd                                       
+        -ddddddsddddddsddddddsdddddhydddddyhdddddsddddddsdddddd+oooooosdddddd                                       
+        `::::::-::::::-::::::-::::::-:::::-::::::-::::::-::::::.-------:::::: "
+        puts "Loading...."
+        sleep (0.09)
+        system ("clear")
+    end 
+
+    def self.frame_ten
+        puts "                                                                                
+        -ddddddsddddddsddddddsdddddhydddddyhdddddsdddddhsdddddhsdddddh+oooooo                                       
+        -ddddddsddddddsddddddsdddddhydddddyhdddddsddddddsddddddsdddddd+oooooo                                       
+        -ddddddsddddddsddddddsdddddhydddddyhdddddsddddddsddddddsdddddd+oooooo                                       
+        `::::::-::::::-::::::-::::::-:::::-::::::-::::::-::::::-::::::.------"
+        puts "Loading...."
+        sleep (0.09)
+        system ("clear")
+    end 
+
+    def self.animation
+        5.times do
+          Loading.frame_one
+          Loading.frame_two
+          Loading.frame_three
+          Loading.frame_four
+          Loading.frame_six
+          Loading.frame_seven
+          Loading.frame_eight
+        end
+      end
+
+      def self.go
+        system ("clear")
+        puts "\n\n💃🏻 TIME TO HAVE SOME FUN 🕺🏿 \n\n"
+        self.animation
+      end 
+
+end 
