2aa05ecb26a38605113bf9e27fbf6e9a14de6fc0
ejixxer999 <lehman.ethan2@gmail.com>
2020-08-08 20:31:43 -0400

    instances containing info pulled from api saved into all array

--- lib/Resturants.rb
+++ lib/Resturants.rb
@@ -1,3 +1,23 @@
-class Resturants
-    
+class Restaurant
+    attr_accessor :name, :phone, :address, :hours, :range, :type
+
+    @@all = []
+
+    def initialize(attr_hash)
+        @name = attr_hash[:name]
+        @phone = attr_hash[:phone]
+        @address = attr_hash[:address]
+        @hours = attr_hash[:hours]
+        @type = attr_hash[:type]
+        @range = attr_hash[:range]
+        @@all << self
+        
+    end 
+
+    def self.all 
+        @@all 
+        
+    end 
+
+
 end 
