61767be93030ab6be19216beebe2b0f70c2770e9
ejixxer999 <lehman.ethan2@gmail.com>
2020-08-09 01:55:10 -0400

    added loading.rb file

--- config/ip_finder.rb
+++ config/ip_finder.rb
@@ -12,7 +12,7 @@ Bundler.require
 
 require_relative "../lib/User"
 require_relative "../lib/Resturants"
-
+require_relative "../lib/loading"
 require_relative "../lib/api"
 require_relative "../lib/cli"
 
